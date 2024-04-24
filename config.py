21ad6024ff4bf6fdd9306882ce4feb7a58cd0172
abhitg342005 <165821097+abhitg342005@users.noreply.github.com>
2024-04-24 16:38:28 +0530

    Update config.py
    Signed-off-by: abhitg342005 <165821097+abhitg342005@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://telegra.ph/file/5bb8b874151e999670164.jpg",
+    "https://github.com/abhitg342005/abhitgbots.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
